f4f37a1d86da87b3ba9938ab19a890ba94e33d5a
BurcuHaddad <burcuhaddad@gmail.com>
2023-10-05 12:36:41 +0300

    Ability to add team members to different types of teams using generic class

--- src/dev/bh/Main.java
+++ src/dev/bh/Main.java
@@ -1,20 +1,69 @@
 package dev.bh;
 
-record BaseballPlayer(String name, String position) {}
+interface Player {
+    String name();
+}
+record BaseballPlayer(String name, String position) implements Player {}
+record FootballPlayer(String name, String position) implements Player {}
+record VolleyballPlayer(String name, String position) implements Player {}
 public class Main {
     public static void main(String[] args) {
-        BaseballTeam phillies = new BaseballTeam("Philadelphia Phillies");
-        BaseballTeam astros = new BaseballTeam("Houston Astros");
+
+        var philly = new Affiliation("city", "Philadelphia, PA", "US");
+        BaseballTeam phillies1 = new BaseballTeam("Philadelphia Phillies");
+        BaseballTeam astros1 = new BaseballTeam("Houston Astros");
+        scoreResult(phillies1,3,astros1,5);
+
+        SportsTeam phillies2 = new SportsTeam("Philadelphia Phillies");
+        SportsTeam astros2 = new SportsTeam("Houston Astros");
+        scoreResult(phillies2,3,astros2,5);
+
+        Team<BaseballPlayer,Affiliation> phillies = new Team<>("Philadelphia Phillies", philly);
+        Team<BaseballPlayer,Affiliation> astros = new Team<>("Houston Astros");
         scoreResult(phillies,3,astros,5);
+
         var harper = new BaseballPlayer("B Harper","Right Fielder");
         var marsh = new BaseballPlayer("B Marsh", "Right Fielder");
         phillies.addTeamMember(harper);
         phillies.addTeamMember(marsh);
+        var guthrie = new BaseballPlayer("D Guthrie", "Center Fielder");
+        phillies.addTeamMember(guthrie);
         phillies.listTeamMembers();
+
+        SportsTeam afc1 = new SportsTeam("Adeilaide Crows");
+        Team<FootballPlayer,String> afc = new Team<>("Adeilaide Crows", "City of Adelaide, South Australia, in AU");
+        var tex = new FootballPlayer("Tex Waler","Centre half forward");
+        afc.addTeamMember(tex);
+        var rory = new FootballPlayer("Rory Laird", "Midfield");
+        afc.addTeamMember(rory);
+        afc.listTeamMembers();
+
+        Team<VolleyballPlayer,Affiliation> adelaide = new Team<>("Adeilade Storm");
+        adelaide.addTeamMember(new VolleyballPlayer("N Robert", "Setter"));
+        adelaide.listTeamMembers();
+
+        var canberra = new Team<VolleyballPlayer,Affiliation>("Canberra Heat");
+        canberra.addTeamMember(new VolleyballPlayer("B Black", "Opposite"));
+        canberra.listTeamMembers();
+        scoreResult(canberra, 0, adelaide,1);
+
+        //Team<Integer> melbourneVB = new Team<>("Melbourne Vipers");
     }
 
     public static void scoreResult(BaseballTeam team1, int t1_score,
                                    BaseballTeam team2, int t2_score) {
+        String message = team1.setScore(t1_score,t2_score);
+        team2.setScore(t2_score, t1_score);
+        System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+    public static void scoreResult(SportsTeam team1, int t1_score,
+                                   SportsTeam team2, int t2_score) {
+        String message = team1.setScore(t1_score,t2_score);
+        team2.setScore(t2_score, t1_score);
+        System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+    public static void scoreResult(Team team1, int t1_score,
+                                   Team team2, int t2_score) {
         String message = team1.setScore(t1_score,t2_score);
         team2.setScore(t2_score, t1_score);
         System.out.printf("%s %s %s %n", team1, message, team2);
